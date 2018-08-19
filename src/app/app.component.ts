7d8920fcfffa9a693e88fa9ad8072be8ce9ae717
zarbon001 <zarbon001@hotmail.com>
2018-08-19 13:50:34 -0400

    Changes after review
    Changed buttons from sorting by each column to sorting by each position
    
    Added reset button to set the table back to original state
    
    Added logic to auto generate position buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,56 +2,33 @@ import { Component, OnInit } from '@angular/core';
 import { InmemoryService } from './inmemory.service';
 import { IPlayer } from './player';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [InmemoryService]
+  providers: [InmemoryService]  
 })
 export class AppComponent {
 
 player: IPlayer = null;
+origilnalPlayerList: Iplayer[] = [];
 players: IPlayer[] = [];
-
-filteredPlayers: IPlayer[];
-sortType: string;
-sortReverse: boolean = false;
+postions: IPlayer[] = [];
 
   constructor(inmemoryService: InmemoryService) {
+  this.origilnalPlayerList = inmemoryService.getPlayers();
   this.players = inmemoryService.getPlayers();
+  this.postions = inmemoryService.getPlayers();
   }
 
-  ngOnInit(): void {
-  this.filteredPlayers = this.players;
-  this.sortPlayers('id');
+  filter(type){
+    this.players = this.origilnalPlayerList.filter((player: IPlayer) => player.position === type); 
+    return this.players;   
   }
 
-  sortPlayers(property) {
-        this.sortType = property;
-        this.sortReverse = !this.sortReverse;
-        this.filteredPlayers.sort(this.dynamicSort(property));
-    }
-
-    dynamicSort(property) {
-        let sortPlayer = -1;
-
-        if (this.sortReverse)
-            sortPlayer = 1;
-
-        return function (a, b) {
-            let result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
-
-            return result * sortPlayer;
-        }
-    }
-
-    filterPlayers(search: string) {
-        this.filteredPlayers = this.players.filter(o =>
-            Object.keys(o).some(k => {
-                if (typeof o[k] === 'string')
-                    return o[k].toLowerCase().includes(search.toLowerCase());
-            }));
-    }
-
-  
+  reset(){
+    this.players = this.origilnalPlayerList; 
+    return this.players;  
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UniquePipe } from './unique.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UniquePipe
   ],
   imports: [
     BrowserModule
