2325bd0ef9a0925f512fad9bf1728692835230fc
nthrive\alynn <alynn@nthrive.com>
2019-07-16 14:02:06 -0400

    Added observable portion to test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +9,4 @@ import { Component } from '@angular/core';
 export class AppComponent {
   constructor() {
   }
-
 }
